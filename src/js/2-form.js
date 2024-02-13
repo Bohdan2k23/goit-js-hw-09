3d52c3d672999d849a32cfffcf62bc8a9a24a65e
Bohdan2k23 <tivonuk2@gmail.com>
2024-02-14 00:34:19 +0100

    update

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -2,9 +2,17 @@ const form = document.querySelector('.feedback-form');
 const email = form.elements.email;
 const message = form.elements.message;
 const key = 'feedback-form-state';
+const save = localStorage.getItem(key);
 
-// Оновлена функція для зберігання даних у локальному сховищі
-function saveFormData() {
+if (save) {
+  const obj = JSON.parse(save);
+  email.value = obj.email;
+  message.value = obj.message;
+}
+
+function onInput(ev) {
+  if (!email.value.trim() && !message.value.trim()) return;
+
   localStorage.setItem(
     key,
     JSON.stringify({
@@ -14,37 +22,17 @@ function saveFormData() {
   );
 }
 
-// Функція валідації форми
-function validateForm() {
+function onSubmit(ev) {
+  ev.preventDefault();
   if (!email.value.trim() || !message.value.trim()) {
     alert('Будь ласка, заповніть усі поля перед відправленням форми.');
-    return false;
+    return;
   }
-  return true;
+
+  console.log(JSON.parse(localStorage.getItem(key)));
+  localStorage.removeItem(key);
+  email.value = '';
+  message.value = '';
 }
-
-// Завантаження збережених даних при завантаженні сторінки
-window.addEventListener('load', function () {
-  const save = localStorage.getItem(key);
-  if (save) {
-    const obj = JSON.parse(save);
-    email.value = obj.email;
-    message.value = obj.message;
-  }
-});
-
-// Збереження даних при кожній зміні в полі вводу
-form.addEventListener('input', function () {
-  saveFormData();
-});
-
-// Відправка форми
-form.addEventListener('submit', function (ev) {
-  ev.preventDefault();
-  if (validateForm()) {
-    console.log(JSON.parse(localStorage.getItem(key)));
-    localStorage.removeItem(key);
-    email.value = '';
-    message.value = '';
-  }
-});
+form.addEventListener('input', onInput);
+form.addEventListener('submit', onSubmit);
